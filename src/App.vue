46cb9a6cf984fc224a1d1abb33e56c0040d394a4
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-03-27 00:12:45 -0300

    feat: create calendar base structure

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,89 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    Calendar
+        <div class="week-days">
+          <p>Sunday</p>
+          <p>Monday</p>
+          <p>Tuesday</p>
+          <p>Wednesday</p>
+          <p>Thursday</p>
+          <p>Friday</p>
+          <p>Saturday</p>
+          <CalendarDay
+            v-for="(days, index) in totalDays"
+            :key="days"
+            :class="{ 'current-day': isCurrentDay(days) }"
+            :style="{ gridColumnStart: index === 0 ? startDayColumn : 'auto' }"
+          >
+              {{  days  }}
+          </CalendarDay>
+        </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+import { defineComponent } from 'vue';
+import CalendarDay from './components/CalendarDay.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    CalendarDay,
+  },
+  data() {
+    return {
+      totalDays: 0,
+      currentDate: new Date(),
+    };
+  },
+  methods: {
+    getDays(year, month) {
+      return new Date(year, month, 0).getDate();
+    },
+    isCurrentDay(days) {
+      if (this.currentDate.getDate() === days) {
+        return true;
+      }
+      return false;
+    },
+  },
+  computed: {
+    startDayColumn() {
+      const firstDayOfWeek = new Date(this.currentYear, this.currentMonth, 1).getDay();
+      return firstDayOfWeek + 1; // Add 1 to make it 1-indexed for grid-column-start
+    },
+    currentMonth() {
+      return this.currentDate.getMonth();
+    },
+    currentYear() {
+      return this.currentDate.getFullYear();
+    },
+  },
+  beforeMount() {
+    console.log(this.currentMonth);
+    console.log(this.currentYear);
+    this.totalDays = this.getDays(2023, 3);
+  },
+});
+</script>
+
+<style>
+.week-days {
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  grid-template-rows: auto;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.current-day {
+  background-color: gray;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
---
+++ src/components/CalendarDay.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="calendar-day">
+    <span class="calendar-day--number">
+      <slot></slot>
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CalendarDay',
+});
+</script>
+<style>
+.calendar-day {
+  padding: 1rem;
+  border: 1px solid black;
+  position: relative;
+}
+
+.calendar-day--number {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.calendar-day:hover:before {
+  content: "+";
+}
+</style>
