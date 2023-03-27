12c9ac6e98dd1fd6a0e657cf6b459e54b0aecb97
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-03-27 14:04:57 -0300

    chore: improve reminder list title

--- src/components/RemindersList.vue
+++ src/components/RemindersList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="reminder-list">
-    <h2>List of Reminders from 23 March 2023</h2>
+    <h2>Reminders from 23 March 2023</h2>
     <ul>
       <li>Do something, Caragua - 21:30</li>
       <li>Do other thing, Ubatuba - 21:30</li>
