6d1dc46966a3603f73101ee2f86ed6c6a55b7239
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-03-27 17:31:43 -0300

    chore: manipulate date in store and not the components

--- src/stores/reminderStore.js
+++ src/stores/reminderStore.js
@@ -2,6 +2,11 @@ import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
 export const useReminderStore = defineStore('reminder', () => {
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth();
+  const currentYear = currentDate.getFullYear();
+  const totalDaysInCurrentMonth = new Date(currentYear, currentMonth+1, 0).getDate();
+
   const reminders = ref([
     {
       date: new Date(2023, 2, 31),
@@ -9,12 +14,24 @@ export const useReminderStore = defineStore('reminder', () => {
       text: 'Gabriel Gomes gets the job',
       city: 'Caraguatatuba',
       color: 'green'
-    }
+    },
+    {
+      date: new Date(2023, 2, 2),
+      time: '12:00',
+      text: 'Gabriel Gomes makes the test',
+      city: 'Sao Paulo',
+      color: 'black'
+    },
   ]);
 
   function addReminder(reminder) {
     reminders.value.push(reminder);
   }
 
-  return { reminders, addReminder }
+  function remindersOfDate(date) {
+    const remindersOfDay = reminders.value.filter((reminder) => new Date(reminder.date).getTime() === date.getTime());
+    return remindersOfDay
+  }
+
+  return { reminders, addReminder, totalDaysInCurrentMonth, remindersOfDate }
 })
