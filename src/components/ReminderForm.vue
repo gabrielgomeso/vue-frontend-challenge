6e399306c44be2f5004c5b920a6ee9d3522d4202
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-03-27 18:44:50 -0300

    chore: create small validation to reminder form

--- src/components/ReminderForm.vue
+++ src/components/ReminderForm.vue
@@ -1,32 +1,34 @@
 <template>
   <div class="reminder-form">
     <h2>Create new reminder</h2>
-    <label class="reminder-form__label" for="reminder">
-      Reminder Date
-      <input type="date" name="reminder-date" v-model="reminderDate">
-    </label>
+    <form @submit.prevent>
+      <label class="reminder-form__label" for="reminder">
+        Reminder Date
+        <input type="date" name="reminder-date" v-model="reminderDate" required>
+      </label>
 
-    <label class="reminder-form__label" for="reminder">
-      Reminder Time
-      <input type="time" name="reminder-time" v-model="reminderTime">
-    </label>
+      <label class="reminder-form__label" for="reminder">
+        Reminder Time
+        <input type="time" name="reminder-time" v-model="reminderTime" required>
+      </label>
 
-    <label class="reminder-form__label" for="reminder">
-      Reminder
-      <input type="text" name="reminder" v-model="reminderText" placeholder="Write what you want to remind!" maxlength="30">
-    </label>
+      <label class="reminder-form__label" for="reminder">
+        Reminder
+        <input type="text" name="reminder" v-model="reminderText" placeholder="Write what you want to remind!" maxlength="30" required>
+      </label>
 
-    <label class="reminder-form__label" for="city">
-      City
-      <input type="text" name="city" v-model="reminderCity" placeholder="Write the city you want to add a reminder for!">
-    </label>
+      <label class="reminder-form__label" for="city">
+        City
+        <input type="text" name="city" v-model="reminderCity" placeholder="Write the city you want to add a reminder for!" required>
+      </label>
 
-    <label class="reminder-form__label" for="reminder-color">
-      Reminder Color
-      <input type="color" name="reminder-color" v-model="reminderColor">
-    </label>
+      <label class="reminder-form__label" for="reminder-color">
+        Reminder Color
+        <input type="color" name="reminder-color" v-model="reminderColor">
+      </label>
 
-    <button type="button" @click="addReminder()">Add reminder</button>
+      <button type="submit" @click="addReminder()">Add reminder</button>
+    </form>
   </div>
 </template>
 
@@ -47,11 +49,12 @@ export default defineComponent({
   },
   methods: {
     addReminder() {
-      const { addReminder } = useReminderStore();
-      const [year, month, day] = this.reminderDate.split('-');
-      const date = new Date(year, month - 1, day);
+      if(this.reminderTime != null && this.reminderText != '' && this.reminderCity != '' && this.reminderDate != null) {
+        const { addReminder } = useReminderStore();
+        const [year, month, day] = this.reminderDate.split('-');
+        const date = new Date(year, month - 1, day);
 
-      let newReminder = {
+        let newReminder = {
         date: date,
         time: this.reminderTime,
         text: this.reminderText,
@@ -59,19 +62,24 @@ export default defineComponent({
         color: this.reminderColor
       };
 
-      addReminder(newReminder)
+        addReminder(newReminder)
 
-      this.reminderText = ''
-      this.reminderCity = ''
-      this.reminderColor = 'green'
-      this.reminderDate = null
-      this.reminderTime = null
+        this.reminderText = ''
+        this.reminderCity = ''
+        this.reminderColor = 'green'
+        this.reminderDate = null
+        this.reminderTime = null
+      }
     },
   },
 });
 </script>
 
 <style>
+.reminder-form {
+  padding: 1rem;
+}
+
 .reminder-form,
 .reminder-form__label
 {
