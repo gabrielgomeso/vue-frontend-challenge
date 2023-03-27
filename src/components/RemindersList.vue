cb5001a2ef8d61d4cb5ea0fa2f2aa5c155ac0e79
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-03-27 15:03:28 -0300

    chore: use store in reminders list

--- src/components/RemindersList.vue
+++ src/components/RemindersList.vue
@@ -1,20 +1,44 @@
 <template>
-  <div class="reminder-list">
+  <section class="reminder-list__body">
     <h2>Reminders from 23 March 2023</h2>
-    <ul>
-      <li>Do something, Caragua - 21:30</li>
-      <li>Do other thing, Ubatuba - 21:30</li>
-      <li>Do a backflip, Ilhabela - 21:30</li>
-      <li>Make bread, Sao Paulo - 21:30</li>
-      <li>Do something, Caragua - 21:30</li>
-    </ul>
-  </div>
+    <div class="reminder-list__list">
+      <span
+        class="reminder-list__reminder"
+        v-for="(reminder, index) in reminders"
+        :key="index"
+      >
+        <span class="reminder-list__reminder--color" :style="{ backgroundColor: reminder.color }"></span>
+        {{ reminder.text }} - {{  reminder.time }} - {{ reminder.date }}
+        At {{  reminder.city }}
+      </span>
+    </div>
+  </section>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import { useReminderStore } from '../stores/reminderStore';
 
 export default defineComponent({
   name: 'ReminderList',
+  computed: {
+    reminders() {
+      const { reminders } = useReminderStore();
+      return reminders;
+    }
+  }
 });
 </script>
+
+<style>
+.reminder-list__list {
+  display: flex;
+  flex-direction: column;
+}
+.reminder-list__reminder--color {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+}
+
+</style>
