962d110c8ec165810b102128d39ac281c3b574c4
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-03-28 11:41:15 -0300

    test: create ReminderForm spec and test add new reminder

---
+++ src/components/__tests__/ReminderForm.spec.js
@@ -0,0 +1,45 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import { setActivePinia, createPinia } from 'pinia'
+import { useReminderStore } from '../../stores/reminderStore'
+import ReminderForm from '../ReminderForm.vue'
+
+describe('ReminderForm', () => {
+  let store
+
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  it('renders properly', () => {
+    const wrapper = mount(ReminderForm, { global: { plugins: [store] } });
+    expect(wrapper.text()).toContain('Create new reminder')
+  })
+
+  it('allows user to add a new reminder', async () => {
+    const { reminders } = useReminderStore()
+
+    const wrapper = mount(ReminderForm, {
+      global: {
+        plugins: [store],
+      },
+    })
+
+    await wrapper.find('[name="reminder-date"]').setValue('2023-03-28')
+    await wrapper.find('[name="reminder-time"]').setValue('12:00')
+    await wrapper.find('[name="reminder"]').setValue('Test reminder')
+    await wrapper.find('[name="city"]').setValue('Test city')
+
+    await wrapper.find('[type="submit"]').trigger('click')
+
+    expect(reminders[2]).toEqual({
+      id: 3,
+      date: new Date(2023, 2, 28),
+      time: '12:00',
+      text: 'Test reminder',
+      city: 'Test city',
+      color: 'black'
+    })
+  })
+})
