f90d17f0ee2d97871eb2a58983372124ebca9cd3
Gabriel Gomes <gabrielogomes1@gmail.com>
2023-03-28 17:14:05 -0300

    style: clear scaffold unused css

--- src/App.vue
+++ src/App.vue
@@ -23,14 +23,7 @@ export default defineComponent({
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 
 .calendar-actions {
   margin-top: 2rem;
